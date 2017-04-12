c32a6bf6de44e5c20ecc97865c122de4932b67e9
Chris Tang <cwolf0913@gmail.com>
2017-04-12 08:52:56 -0400

    added Model

--- src/main/java/com/christang/SheldonController.java
+++ src/main/java/com/christang/SheldonController.java
@@ -1,13 +1,17 @@
 package com.christang;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDateTime;
 
 @Controller
 public class SheldonController {
 
     @RequestMapping ("/BigBang")
-    public String allTheGifs(){
+    public String allTheGifs(Model model){
+        model.addAttribute("time", LocalDateTime.now());
         return "bigbangtheory";
     }
 }
